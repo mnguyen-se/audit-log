494c489e135097a783a0ea7ecc8eb8dc748c3637
mnguyen-se <minhnguyen08112004@gmail.com>
2026-07-09 11:39:12 +0700

    BE - [DELETE] delete kafka

--- BE/audit-service/src/main/java/com/example/audit_service/service/AuditLogConsumerService.java
+++ BE/audit-service/src/main/java/com/example/audit_service/service/AuditLogConsumerService.java
@@ -1,9 +1,7 @@
 package com.example.audit_service.service;
 
-import com.example.audit_service.dto.AuditLogMessage;
 import com.example.audit_service.dto.req.AuditLogRequest;
 
 public interface AuditLogConsumerService {
-    public void consume(AuditLogMessage message);
     public void createLog(AuditLogRequest auditLogRequest);
 }
--- BE/audit-service/src/main/java/com/example/audit_service/serviceImpl/AuditLogServiceImpl.java
+++ BE/audit-service/src/main/java/com/example/audit_service/serviceImpl/AuditLogServiceImpl.java
@@ -1,36 +1,18 @@
 package com.example.audit_service.serviceImpl;
 
-import com.example.audit_service.dto.AuditLogMessage;
 import com.example.audit_service.dto.req.AuditLogRequest;
 import com.example.audit_service.entity.AuditLog;
 import com.example.audit_service.repository.AuditLogRepository;
 import com.example.audit_service.service.AuditLogConsumerService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
-
-import java.time.LocalDateTime;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class AuditLogServiceImpl implements AuditLogConsumerService {
     private final AuditLogRepository auditLogRepository;
-
-    @KafkaListener(topics = "${app.kafka.topic}", groupId = "${app.kafka.group-id}")
-    public void consume(AuditLogMessage message) {
-        log.info("Nhan duoc audit log tu Kafka: {} - {}", message.getAction(), message.getEntity());
-
-        AuditLog entity = AuditLog.builder()
-                .action(message.getAction())
-                .entity(message.getEntity())
-                .detail(message.getDetail())
-                .username(message.getUsername())
-                .build();
-
-        auditLogRepository.save(entity);
-    }
 
     @Override
     public void createLog(AuditLogRequest auditLogRequest) {
